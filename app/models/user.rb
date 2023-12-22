0c2453d5f46bc12fad7824f7bb2332312d43ef61
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2023-12-22 16:09:02 +0900

    バリデーション設定2回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, on: :create
-  validates :password, confirmation: true
   validates :username, presence: true
   validates :profile, presence: true
   validates :affiliation, presence: true
