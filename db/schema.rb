25cda22ec7ae26fb45e2b5cf3cd531a8e4202128
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2023-12-25 12:47:31 +0900

    テーブル作成　アソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_25_022241) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_25_031610) do
+  create_table "prototypes", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy"
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_25_022241) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "prototypes", "users"
 end
