f5b9c3fdb5527e2043db5545420f29ff86c12600
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2023-12-25 13:38:10 +0900

    Active Storage導入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
-  def index
+  def prototype_params
+    params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
