9414a77731e7cbdd6c6ce46a7bd284d5cf107843
yuta-chi <152576167+yuta-chi@users.noreply.github.com>
2023-12-25 15:16:36 +0900

    アソシエーション　Active storage  ビューの配置

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,18 @@
 class PrototypesController < ApplicationController
+  def index
+
+  end
+
+  def new
+    
+  end
+
+  def create
+
+  end
+  
+  
+  
   def prototype_params
     params.require(:prototype).permit(:content, :image).merge(user_id: current_user.id)
   end
